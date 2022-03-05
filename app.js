770d948267b4ebd6d5dc45f9854df5374153d84c
Steven Sares <steven.sares@outlook.com>
2022-03-05 15:14:16 -0700

    fixed typo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 //EXPRESS
 const express = require("express")
 const app = express()
-const port = procss.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 //EJS
 app.set("view engine", "ejs")
